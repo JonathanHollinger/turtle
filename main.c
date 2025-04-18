e32140d86cd638551149f0038a74558298346e52
Jonathan Hollinger <Hollinjf@dukes.jmu.edu>
2025-04-18 13:47:13 -0400

    adding some other starter code

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 
 int main(int argc, char *argv[]) {
   printf("Hello world!\n");
+  if (argc == 1) {
+    printf("One argument");
+  } else {
+    for (int i = 1; i < argc; i++) {
+      printf("%s ", argv[i]);
+    }
+  }
     // Code to be executed
     return 0;
   }
